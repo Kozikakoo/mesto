5192867f364abf9b89658f739727b19b7a30e896
Anastasiya Nagornaya <kozikakoo@yandex.ru>
2021-10-23 16:14:49 +0300

    feat: Создать функции для работы с массивом

--- scripts/index.js
+++ scripts/index.js
@@ -19,16 +19,14 @@ function closePopup() {
   popup.classList.remove('popup_open')
 };
 
-
-
 function submitForm(event) {
   event.preventDefault();
-  
+
   profileName.textContent = popupPerson.value
   profileDesc.textContent = popupDesc.value
-  
+
   closePopup();
-  }
+}
 
 
 editButton.addEventListener('click', openPopup);
@@ -37,5 +35,47 @@ popupClose.addEventListener('click', closePopup);
 
 form.addEventListener('submit', submitForm);
 
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
 
+const cardsElement = document.querySelector('.cards');
+const templateItem = document.querySelector('.template').content;
+
+initialCards.forEach(appendCard)
+
+function createCard(item) {
+  const element = templateItem.querySelector('.card').cloneNode(true);
+  element.querySelector('.card__title').innerText = item.name;
+  element.querySelector('.card__image').src = item.link;
+  return element;
+}
+
+function appendCard(item) {
+  const element = createCard(item);
+  cardsElement.append(element);
+}
 
