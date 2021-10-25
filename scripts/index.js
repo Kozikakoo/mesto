9f50d1919e9cac29a04a39d02eb5b526d77b795a
Anastasiya Nagornaya <kozikakoo@yandex.ru>
2021-10-25 19:10:50 +0300

    feat: Дописать скрипт к 5 спринту

--- scripts/index.js
+++ scripts/index.js
@@ -33,13 +33,25 @@ initialCards.forEach(appendCard)
 function createCard(item) {
   const element = templateItem.querySelector('.card').cloneNode(true);
   element.querySelector('.card__title').innerText = item.name;
-  element.querySelector('.card__image').src = item.link;
+  element.querySelector('.card__image').style.backgroundImage = `url(${item.link})`;
+  element.querySelector('.card__delete').addEventListener('click', (event) => { event.target.closest(".card").remove() });
+  element.querySelector('.card__like').addEventListener('click', (event) => { event.target.classList.toggle('card__like_active') });
+  element.querySelector('.card__image').addEventListener('click', () => {
+    document.querySelector('.popup-image__img').src = item.link;
+    document.querySelector('.popup-image').classList.add('popup_open');
+    document.querySelector('.popup-image').classList.add('popup_transition');
+  })
   return element;
 }
 
 function appendCard(item) {
   const element = createCard(item);
   cardsElement.append(element);
+}
+
+function prependCard(item) {
+  const element = createCard(item);
+  cardsElement.prepend(element);
 }
 
 
@@ -49,12 +61,18 @@ const popupCloseEditProfile = document.querySelector('.popup__close_edit-profile
 const popupCloseAddPlace = document.querySelector('.popup__close_add-place');
 const editButton = document.querySelector('.profile__edit');
 const addButton = document.querySelector('.profile__add')
-const Editform = document.querySelector('.popup__form_edit-profile');
+const editForm = document.querySelector('.popup__form_edit-profile');
 let profileName = document.querySelector('.profile__name');
-let profileDesc = document.querySelector('.profile__desc')
+let profileDesc = document.querySelector('.profile__desc');
 let popupEditPerson = document.querySelector('.popup__form-field_name_person');
 let popupEditDesc = document.querySelector('.popup__form-field_name_desc');
 
+const popupAddNamePlace = document.querySelector('.popup__form-field_place_name');
+const popupAddLinkPlace = document.querySelector('.popup__form-field_place_link');
+const addForm = document.querySelector('.popup__form_add-place');
+
+const popupCloseImage = document.querySelector('.popup__close_image');
+const popupImage = document.querySelector('.popup-image');
 
 function openPopupEdit() {
   popupEdit.classList.add('popup_open');
@@ -74,6 +92,10 @@ function closeEditPopup() {
   popupEdit.classList.remove('popup_open');
 }
 
+function closePopupImage() {
+  popupImage.classList.remove('popup_open');
+}
+
 function submitFormEditPopup(event) {
   event.preventDefault();
 
@@ -83,6 +105,25 @@ function submitFormEditPopup(event) {
   closeEditPopup();
 }
 
+function submitFormAddPopup(event) {
+  event.preventDefault();
+  const name = popupAddNamePlace.value;
+  const link = popupAddLinkPlace.value;
+  const item = {
+    name: name,
+    link: link
+  }
+  prependCard(item);
+  event.target.reset();
+  closeAddPopup();
+}
+
+
+
+
+window.addEventListener('load', () => {
+  document.querySelectorAll('.popup').forEach((item) => item.classList.add('popup_transition'))
+})
 
 
 editButton.addEventListener('click', openPopupEdit);
@@ -93,6 +134,10 @@ popupCloseEditProfile.addEventListener('click', closeEditPopup);
 
 popupCloseAddPlace.addEventListener('click', closeAddPopup);
 
-Editform.addEventListener('submit', submitFormEditPopup);
+popupCloseImage.addEventListener('click', closePopupImage);
+
+editForm.addEventListener('submit', submitFormEditPopup);
+
+addForm.addEventListener('submit', submitFormAddPopup);
 
 
